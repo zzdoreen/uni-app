76b3bf516c48324d132e147d7b1f98b125f5f391
zzdoreen <zz1061831341@qq.com>
2021-12-01 17:27:38 +0800

    feat：update chat room

--- src/subpages/chat/chat.vue
+++ src/subpages/chat/chat.vue
@@ -3,51 +3,23 @@
 
 		<!-- 聊天列表 -->
 		<view class="list">
-			<view class="left">
-				<image class="avatar" src="../../static/logo.png" mode="widthFix"></image>
-				<view class="text">
-					<text>正在输入...</text>
+			<view :class="item.isMe?'right':'left'" v-for="(item,key) in chatList" :key='key'>
+				<image v-if="!item.isMe" class="avatar" src="../../static/logo.png" mode="widthFix"></image>
+				<view :class="item.type==='text'?'text':'image_info'">
+					<text v-if="item.type==='text'">{{item.content}}</text>
+					<image v-else :src="item.content"></image>
 				</view>
-			</view>
-			<view class="right">
-				<view class="text">
-					<text>正在输入...</text>
-				</view>
-				<image class="avatar" src="../../static/logo.png" mode="widthFix"></image>
-
-			</view>
-			<view class="left">
-				<image class="avatar" src="../../static/logo.png" mode="widthFix"></image>
-				<view class="text">
-					<text>正在输入...</text>
-				</view>
-			</view>
-			<view class="right">
-				<view class="text">
-					<text>正在输入...</text>
-				</view>
-				<image class="avatar" src="../../static/logo.png" mode="widthFix"></image>
-
-			</view>
-			<view class="left">
-				<image class="avatar" src="../../static/logo.png" mode="widthFix"></image>
-				<view class="text">
-					<text>正在输入...</text>
-				</view>
-			</view>
-			<view class="right">
-				<view class="text">
-					<text>正在输入...</text>
-				</view>
-				<image class="avatar" src="../../static/logo.png" mode="widthFix"></image>
-
+				<image v-if="item.isMe" class="avatar" src="../../static/logo.png" mode="widthFix"></image>
 			</view>
 		</view>
+
 		<!-- 聊天输入 -->
 		<view class="bottom">
-			<input class="input_text" type='text' placeholder="请输入内容" />
-			<!-- <uni-icons type=""></uni-icons> -->
-			<button size="small">发送</button>
+			<input class="input_text" type='text' placeholder="请输入内容" v-model="content" />
+			<image class="bottom_image" mode="widthFix" @click="handleChooseImage"
+				src="https://img0.baidu.com/it/u=2862203040,3186546841&fm=26&fmt=auto">
+			</image>
+			<button size="small" @click="handleSend">发送</button>
 		</view>
 
 	</view>
@@ -55,29 +27,114 @@
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				content: '',
+				chatList: [{
+						isMe: false,
+						type: 'text',
+						content: 'aaa'
+					},
+					{
+						isMe: true,
+						type: 'image',
+						content: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F16%2F20170516135450_vsUG2.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1640937609&t=83493b8e2a3460f6ba6de209749cb1a0'
+					},
+					{
+						isMe: false,
+						type: 'text',
+						content: 'bbb'
+					},
+					{
+						isMe: true,
+						type: 'text',
+						content: 'bbb'
+					},
+					{
+						isMe: true,
+						type: 'text',
+						content: 'bbb'
+					},
+				]
+			}
+		},
+		onShow() {
+			if (uni.getStorageSync('data')) {
+				this.chatList = JSON.parse(uni.getStorageSync('data'))
+			}
+			setTimeout(() => {
+				uni.pageScrollTo({
+					scrollTop: 9999999999999999999999,
+					duration: 0
+				})
+			}, 10)
+
+		},
+		methods: {
+			handleChooseImage() {
+				uni.chooseImage({
+					count: 1,
+					sizeType: ['original', 'compressed'],
+					sourceType: ['album', 'camera'],
+					success: (res) => {
+
+						this.chatList.push({
+							isMe: true,
+							type: 'image',
+							content: res.tempFiles[0].path
+						})
+						uni.setStorageSync('data', JSON.stringify(this.chatList))
+						setTimeout(() => {
+							uni.pageScrollTo({
+								scrollTop: 9999999999999999999999,
+								duration: 0
+							})
+						}, 10)
+
+					}
+				})
+			},
+			handleSend() {
+				this.chatList.push({
+					isMe: true,
+					type: 'text',
+					content: this.content
+				})
+				this.content = ''
+				uni.setStorageSync('data', JSON.stringify(this.chatList))
+				setTimeout(() => {
+					uni.pageScrollTo({
+						scrollTop: 9999999999999999999999,
+						duration: 0
+					})
+				}, 10)
+			}
+		}
+	}
 </script>
 
 <style>
 	.content {
-		background-color: #C0C0C0;
-		width: 100vw;
-		height: 100vh;
-		overflow: hidden;
+		background-color: #EBEBEB;
+		height: auto;
+		overflow: scroll;
 	}
 
 	.list {
-		border: 1px solid black;
 		width: 100%;
-		height: calc(100% - 40rpx);
+		height: auto;
 		padding: 30rpx 10rpx 10rpx 10rpx;
 		box-sizing: content-box;
 		display: flex;
 		flex-direction: column;
+		margin-bottom: 100px;
 	}
 
 	.left,
 	.right {
 		display: flex;
+		position: relative;
 	}
 
 	.right {
@@ -89,7 +146,7 @@
 		height: 80rpx;
 		border-radius: 50%;
 		border: 2px solid #F1F1F1;
-		margin: 0 10rpx 0rpx 10rpx;
+		margin: 0 10rpx 0rpx 5rpx;
 	}
 
 	.text {
@@ -98,7 +155,55 @@
 		height: 60rpx;
 		border-radius: 10px;
 		line-height: 60rpx;
-		padding: 5px;
+		padding: 3px 15px;
+		margin: 10px 10px 0 10px;
+	}
+
+	.image_info {
+		background: white;
+		font-size: 12px;
+		border-radius: 10px;
+		line-height: 60rpx;
+		padding: 3px 10px;
+		margin: 10px 10px 0 10px;
+
+	}
+
+	.right .image_info {
+		background-color: #ABD33D;
+	}
+
+	.image_info image {
+		max-width: 200px;
+	}
+
+
+	.left .text::after,
+	.left .image_info::after {
+		content: '';
+		display: inline-block;
+		position: absolute;
+		left: 105rpx;
+		border: 10px solid white;
+		border-bottom-color: transparent;
+		border-left-color: transparent
+	}
+
+
+	.right .text::after,
+	.right .image_info::after {
+		content: '';
+		display: inline-block;
+		position: absolute;
+		right: 90rpx;
+		border: 10px solid #ABD33D;
+		border-bottom-color: transparent;
+		border-right-color: transparent
+	}
+
+	.right .text {
+		background: #ABD33D;
+		color: white;
 	}
 
 	.bottom {
@@ -116,7 +221,7 @@
 	}
 
 	.input_text {
-		width: 70%;
+		width: 60%;
 		font-size: 12px;
 		border: 1px solid #C0C0C0;
 		border-radius: 5px;
@@ -129,8 +234,14 @@
 		width: 150rpx;
 		font-size: 12px;
 		height: 70rpx;
-		background-color: #ccc;
+		background-color: #ABD33D;
 		color: white;
 
+	}
+
+	.bottom_image {
+		width: 50rpx;
+		hieght: 50rpx;
+		margin: 5px 5px 0 10px;
 	}
 </style>
